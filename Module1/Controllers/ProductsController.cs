50bb881ccffd62fc0ee45f5995fd834e604cf277
the canxi <hoanglongcnttk42b2@gmail.com>
2021-07-19 18:08:53 +0700

    update

---
+++ Module1/Controllers/ProductsController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Module1.Data;
+using Module1.Models;
+using Microsoft.AspNetCore.Http;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Module1.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Products")]
+    public class ProductsController : ControllerBase
+    {
+        private ProductsDbContext productsDbContext;
+
+        public ProductsController(ProductsDbContext _productsDbContext)
+        {
+            productsDbContext = _productsDbContext;
+        }
+        
+        // GET: api/<ProductsController>
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return productsDbContext.Products;
+        }
+
+        // GET api/<ProductsController>/5
+        [HttpGet("{id}", Name = "Get")]
+        public IActionResult Get(int id)
+        {
+           var product = productsDbContext.Products.SingleOrDefault(m => m.Id == id);
+            if(product == null)
+            {
+                return NotFound("No Record Found...");
+            }
+
+            return Ok(product);
+        }
+
+        // POST api/<ProductsController>
+        [HttpPost]
+        public IActionResult Post([FromBody] Product product)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            productsDbContext.Products.Add(product);
+            productsDbContext.SaveChanges(true);
+            return StatusCode(StatusCodes.Status201Created);
+        }
+
+        // PUT api/<ProductsController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ProductsController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
